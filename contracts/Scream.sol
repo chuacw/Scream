f96e10775289e4f985a8c3da7e96d67bea5a57cf
chuacw <1757930+chuacw@users.noreply.github.com>
2017-12-11 20:40:48 +0800

    Removed unnecessary check from costs modifier
    Removed unnecessary check from costs modifier

--- contracts/Scream.sol
+++ contracts/Scream.sol
@@ -4,11 +4,12 @@ contract Scream {
 
   event __Shout(string message);
   event __ShoutFrom(string owner, string message);
+  event __ShoutTo(address indexed recipient, string message);
 
   address public owner;
   uint public creationTime;
   uint public fee;
-  uint public idfee;
+  uint public idFee;
   mapping (address => uint) public balances;
 
   modifier onlyOwner() {
@@ -16,10 +17,21 @@ contract Scream {
     _;
   }
 
+
   modifier costs(uint _amount) {
-    if ((msg.sender != owner) || (balances[msg.sender] < _amount))
-      require (msg.value >= _amount);
-    _;
+
+    if (msg.sender != owner) {
+      require (balances[msg.sender] + msg.value >= _amount);
+    }
+
+    _; // do whatever that costs
+
+    // charge user only after it's been done
+    if (msg.sender != owner) {
+        balances[msg.sender] += msg.value;
+        balances[msg.sender] -= _amount;
+    }
+
   }
 
   modifier onlyAfter(uint _time) {
@@ -31,31 +43,41 @@ contract Scream {
     owner = msg.sender;
     creationTime = now;
     fee = 0.3 finney;   // this is about 10 USD cents
-    idfee = 0.3 finney; // another 10 cents to identify yourself...
+    idFee = 0.3 finney; // another 10 cents to identify yourself...
   }
+
 
   function shout(string message) payable costs(fee) public {
     __Shout(message);
   }
 
-  function shout(string _owner, string message) payable costs(fee+idfee) public {
+  function shout(string _owner, string message) payable costs(fee+idFee) public {
     __ShoutFrom(_owner, message);
   }
+
+  function shout(address recipient, string message) payable costs(fee+idFee) public {
+    __ShoutTo(recipient, message);
+  }
+
 
   function emptyBalance() onlyOwner() public {
     msg.sender.transfer(this.balance);
   }
 
-  function changeOwner(address _newOwner) onlyOwner() onlyAfter(creationTime + 6 weeks) public {
-    owner = _newOwner;
+  function changeOwner(address newOwner) onlyOwner() onlyAfter(creationTime + 6 weeks) public {
+    owner = newOwner;
   }
 
   function changeFee(uint newFee) onlyOwner() public {
     fee = newFee;
   }
 
-  function changeIDfee(uint newIDFee) onlyOwner() public {
-    idfee = newIDFee;
+  function changeIdFee(uint newIdFee) onlyOwner() public {
+    idFee = newIdFee;
+  }
+
+  function() payable public {
+    balances[msg.sender] += msg.value;
   }
 
 }
